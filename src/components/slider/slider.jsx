92b9a1d4e886b9c49f84584ee7d13bbf83570c4c
Andrey Filippov <androsphilippos@gmail.com>
2021-10-18 11:43:04 +0500

    рефакт slider

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,25 +1,33 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+
+import SwiperCore, {Pagination, Autoplay} from 'swiper';
+import {Swiper, SwiperSlide} from 'swiper/react';
 
 import Slide1 from './slide1/slide1';
 import Slide2 from './slide2/slide2';
 import Slide3 from './slide3/slide3';
 
+import 'swiper/swiper.scss';
+import './slider.scss';
+
+SwiperCore.use([Pagination, Autoplay]);
+
 function Slider() {
-
-  const [state, setState] = useState(1);
-
-  useEffect(() => setTimeout(() => setState((s) => s === 3 ? s = 1 : s = s + 1), 4000), [state]);
-
-  switch(state) {
-    case 1:
-      return <Slide1 />;
-    case 2:
-      return <Slide2 />;
-    case 3:
-      return <Slide3 />;
-    default:
-      return null;
-  }
+  return (
+    <div className="slider">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        autoplay={{delay: 4000, disableOnInteraction: false}}
+        loop
+        pagination
+      >
+        <SwiperSlide><Slide1 /></SwiperSlide>
+        <SwiperSlide><Slide2 /></SwiperSlide>
+        <SwiperSlide><Slide3 /></SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
 
 export default Slider;
