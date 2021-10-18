a65caa5e8d99b86cad6088fdbf769f739ca2af5d
Andrey Filippov <androsphilippos@gmail.com>
2021-10-18 11:36:44 +0500

    реализовал компонент swiper-services

---
+++ src/components/services/swiper-services/swiper-services.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import {Swiper, SwiperSlide} from 'swiper/react';
+import SwiperCore, {Pagination} from 'swiper';
+
+import 'swiper/swiper.scss';
+import 'swiper/components/pagination/pagination.scss';
+
+import Deposits from '../deposits/deposits';
+import Credits from '../credits/credits';
+import Insurance from '../insurance/insurance';
+import OnlineServices from '../online-services/online-services';
+
+SwiperCore.use([Pagination]);
+
+function SwiperServices() {
+  return (
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={30}
+      loop
+      pagination
+    >
+      <SwiperSlide><Deposits /></SwiperSlide>
+      <SwiperSlide><Credits /></SwiperSlide>
+      <SwiperSlide><Insurance /></SwiperSlide>
+      <SwiperSlide><OnlineServices /></SwiperSlide>
+    </Swiper>
+  );
+}
+
+export default SwiperServices;
