4d502f0ec1ee56d139f36a016a7caaedc836a80e
Andrey Filippov <androsphilippos@gmail.com>
2021-10-18 11:39:34 +0500

    рефакт app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import PageHeader from '../page-header/page-header';
 import Slider from '../slider/slider';
 import Services from '../services/services';
+import CreditCalc from '../credit-calc/credit-calc';
 import OfficialMap from '../official-map/official-map';
 import PageFooter from '../page-footer/page-footer';
 
@@ -12,6 +13,7 @@ function App() {
       <PageHeader />
       <Slider />
       <Services />
+      <CreditCalc />
       <OfficialMap />
       <PageFooter />
     </>
