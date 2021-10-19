89ee1d17ce38972c3ea4625842ea196700eefeec
Andrey Filippov <androsphilippos@gmail.com>
2021-10-19 14:50:51 +0500

    вынес экспорты в конец файла

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
-export const setNoBodyScroll = () => document.body.style.overflow = 'hidden';
-export const setBodyScroll = () => document.body.style.overflow = 'scroll';
+const setNoBodyScroll = () => document.body.style.overflow = 'hidden';
+const setBodyScroll = () => document.body.style.overflow = 'scroll';
 
-export const getValueNumber = (value) => +value.toString().replace(/[^\d]/g, '');
-export const getCostString = (value) => `${value.toLocaleString()} рублей`;
-export const getPeriodString = (value) => `${value} лет`;
+const getValueNumber = (value) => +value.toString().replace(/[^\d]/g, '');
+const getCostString = (value) => `${value.toLocaleString()} рублей`;
+const getPeriodString = (value) => `${value} лет`;
 
 const MONTHS_IN_YEAR = 12;
 const MOTHER_CAPITAL = 470000;
@@ -23,23 +23,23 @@ const AutocreditRate = {
   TOTAL_INSURANCE: 3.5,
 };
 
-export const getSumMortgage = (cost, initialPayment, isMotherCapital) => {
+const getSumMortgage = (cost, initialPayment, isMotherCapital) => {
   const motherCapital = isMotherCapital ? MOTHER_CAPITAL : 0;
   return getValueNumber(cost) - getValueNumber(initialPayment) - motherCapital;
 };
 
-export const getMortgageRate = (initialPercent) => initialPercent < MortgageRate.CONDITION ? MortgageRate.MAX : MortgageRate.MIN;
+const getMortgageRate = (initialPercent) => initialPercent < MortgageRate.CONDITION ? MortgageRate.MAX : MortgageRate.MIN;
 
-export const getMonthPayment = (cost, rate, period) => {
+const getMonthPayment = (cost, rate, period) => {
   const floatRate = rate / 100 / MONTHS_IN_YEAR;
   return Math.round(cost * (floatRate + floatRate / ((1 + floatRate) ** (getValueNumber(period) * MONTHS_IN_YEAR) - 1)));
 };
 
-export const getIncome = (monthPayment) => Math.round(monthPayment * 100 / MIN_INCOME);
+const getIncome = (monthPayment) => Math.round(monthPayment * 100 / MIN_INCOME);
 
-export const getSumAutocredit = (cost, initialPayment) => getValueNumber(cost) - getValueNumber(initialPayment);
+const getSumAutocredit = (cost, initialPayment) => getValueNumber(cost) - getValueNumber(initialPayment);
 
-export const getAutocreditRate = (cost, isСarInsurance, isLifeInsurance) => {
+const getAutocreditRate = (cost, isСarInsurance, isLifeInsurance) => {
   if (getValueNumber(cost) < CAR_PRICE && (!isСarInsurance && !isLifeInsurance)) {
     return AutocreditRate.MAX1;
   }
@@ -55,4 +55,18 @@ export const getAutocreditRate = (cost, isСarInsurance, isLifeInsurance) => {
   if (isСarInsurance || isLifeInsurance) {
     return AutocreditRate.PART_INSURANCE;
   }
+};
+
+export {
+  setNoBodyScroll,
+  setBodyScroll,
+  getValueNumber,
+  getCostString,
+  getPeriodString,
+  getSumMortgage,
+  getMortgageRate,
+  getMonthPayment,
+  getIncome,
+  getSumAutocredit,
+  getAutocreditRate
 };
